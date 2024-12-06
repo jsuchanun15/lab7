ad596177e6a90b9126b13f37741a8a6685c2f1b1
jsuchanun15 <j.suchanan15@gmail.com>
2024-12-06 13:24:29 +0700

    Update lab7_1.cpp

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    float a  =  20.0, b = 30.0;
+    float c = 4.0; 
+    float output;
+
+    output = a - b/c;
+    cout << "output: " << output << endl;
+    return 0;
+}
